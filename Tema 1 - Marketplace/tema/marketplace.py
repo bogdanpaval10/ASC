f5e72ef5138d0b69c975e32b902b434c4b4aa785
bogdanpaval10 <74198156+bogdanpaval10@users.noreply.github.com>
2023-04-21 16:02:02 +0300

    add unittests

--- Tema 1 - Marketplace/tema/marketplace.py
+++ Tema 1 - Marketplace/tema/marketplace.py
@@ -10,6 +10,8 @@ from threading import Lock, Semaphore, currentThread
 import logging
 from logging.handlers import RotatingFileHandler
 import time
+import unittest
+from .product import Tea, Coffee
 
 
 logging.basicConfig(filename="marketplace.log", level=logging.INFO)
@@ -21,6 +23,10 @@ class Marketplace:
     Class that represents the Marketplace. It's the central part of the implementation.
     The producers and consumers use its methods concurrently.
     """
+
+    nr_producers = int
+    nr_products = int
+    nr_carts = int
 
     def __init__(self, queue_size_per_producer):
         """
@@ -34,13 +40,13 @@ class Marketplace:
         self.producers_lists = {}  # fiecare producator are lista lui de produse
         self.all_products = []  # elemente de tipul tuplu (produs, id_producator)
                                                 # -> toate produsele din magazin
-        self.all_carts = []  # fiecare carucior are lista cu produsele din el
+        self.all_carts = []  # fiecare cart are lista cu produsele din el
         self.nr_producers = 0  # nr total de producatori
         self.nr_products = 0  # nr total de produse din magazin
         self.nr_carts = 0  # nr total de cosuri (consumatori)
 
         self.locker_nr_producers = Lock()  # folosit cand se adauga un producator nou
-        self.locker_all_carts = Lock()  # folosit cand se adauga un cart nouz
+        self.locker_all_carts = Lock()  # folosit cand se adauga un cart nou
         self.sem_order = Semaphore(1)  # folosit la plasarea comenzii
         self.locker_insert_del = Lock()  # folosit la add/remove
 
@@ -60,7 +66,7 @@ class Marketplace:
             self.nr_producers = self.nr_producers + 1  # se adauga noul producator
             self.producers_lists[producer_id] = []  # lista cu produsele producatorului cu prod_id
         except ValueError:
-            self.loggy.info("ERROR: There have been an error, register_producer function!")
+            self.loggy.info("ERROR: There has been an error, register_producer function!")
         finally:
             self.loggy.info("Successfully registered the producer %s.", str(producer_id))
 
@@ -112,7 +118,7 @@ class Marketplace:
             self.nr_carts = self.nr_carts + 1  # se adauga noul cart (consumator)
             self.all_carts.append([])  # lista pt a stoca produsele din cosul cart_id
         except ValueError:
-            self.loggy.info("ERROR: There have been an error creating the new cart!")
+            self.loggy.info("ERROR: There has been an error creating the new cart!")
         finally:
             self.loggy.info("Successfully added the cart %s.", str(cart_id))
 
@@ -174,7 +180,7 @@ class Marketplace:
         """
 
         if int(cart_id) > self.nr_carts:  # daca cosul nu exista (are id > nr total de cosuri)
-            self.loggy.info("ERROR: The cart %s doesn't exist to be removed %s.", str(cart_id),
+            self.loggy.info("ERROR: The cart %s doesn't exist to remov %s.", str(cart_id),
                                                                                     str(product))
             return None
 
@@ -197,7 +203,7 @@ class Marketplace:
 
         self.loggy.info("Successfully removed the product %s from %s.", str(product),
                                                                           str(cart_id))
-        if my_bool is True:  # nu a fost gasit produsul in cos
+        if my_bool is False:  # nu a fost gasit produsul in cos
             self.loggy.info("ERROR: The product %s doesn't exist in %s.", str(product),
                                                                             str(cart_id))
 
@@ -211,23 +217,120 @@ class Marketplace:
         :param cart_id: id cart
         """
 
+        self.loggy.info("At the beginning of place_order %s.", str(cart_id))
+
         if int(cart_id) > self.nr_carts:  # daca cosul nu exista (are id > nr total de cosuri)
             self.loggy.info("ERROR: The cart %s doesn't exist to be ordered.", str(cart_id))
             return None
 
-        self.loggy.info("At the beginning of place_order %s.", str(cart_id))
         list_order = []  # se formeaza o lista cu produsele din cos
         try:
             # se parcurge lista cu produsele consumatorului cart_id
             for curr_cart in self.all_carts[cart_id]:
+                print_message = f"{currentThread().getName()} bought {str(curr_cart[0])}"
+                print(print_message)
                 # adauga doar produsul (nu si id producator - este tuplu)
                 list_order.append(curr_cart[0])
-                print(f"{currentThread().getName()} bought {str(curr_cart[0])}")
             for curr_cart in self.all_carts[cart_id]:  # se sterg produsele din cos
                 self.remove_from_cart(cart_id, curr_cart[0])
         except ValueError:
-            self.loggy.info("ERROR: There have been an error placing the order!")
+            self.loggy.info("ERROR: There has been an error placing the order!")
         finally:
             self.loggy.info("Successfully placed the order %s.", str(cart_id))
 
         return list_order
+
+
+class TestMarketplace(unittest.TestCase):
+    """
+    Class used to test the methods from Marketplace class.
+    """
+    products = []
+    nr_products = 0
+
+    def setUp(self):
+        self.marketplace = Marketplace(queue_size_per_producer=10)
+        for i in range(0, 10):
+            self.products.append(Tea(name='Wild Cherry', price=(5 + i), type='Black'))
+            self.products.append(Coffee(name='Brasil', price=(7 + 2 * i), acidity=(5.09 + i),
+                                        roast_level='MEDIUM'))
+            self.nr_products = self.nr_products + 2
+
+    def test_register_producer(self):
+        """
+        Se adauga 3 producatori, iar daca id-ul asteptat nu este acelasi cu cel returnat,
+        se afiseaza mesajul.
+        """
+
+        for i in range(0, 3):
+            self.assertEqual(self.marketplace.register_producer(), str(i),
+                             "Problem at register producer!")
+
+    def test_publish(self):
+        """
+        Se adauga 3 produse din bancul de produse creat initial. Daca primeste un cos existent
+        atunci se adauga, iar daca este un cos inexistent va primi eroare.
+        """
+
+        producers = []
+        for i in range(0, 3):
+            producers.append(self.marketplace.register_producer())
+
+        for i in range(0, 3):
+            self.assertTrue(self.marketplace.publish(producers[i],
+                                    self.products[self.nr_products - self.nr_products + i]))
+
+        for i in range(4, 6):
+            self.assertFalse(self.marketplace.publish(str(i), self.products[i]))
+
+    def test_new_cart(self):
+        """
+        Se adauga 3 consumatori, iar daca id-ul asteptat nu este acelasi cu cel returnat,
+        se afiseaza mesajul.
+        """
+
+        for i in range(0, 3):
+            self.assertEqual(self.marketplace.new_cart(), i, "Problem at register new cart!")
+
+    def test_add_to_cart(self):
+        """
+        Se creeaza un consumator si se adauga in cos 3 produse din banc, iar aceaste operatii
+        se vor executa cu succes. Apoi se incearca adaugarea unui produs intr-un cos inexistent,
+        ceea ce duce la eroare.
+        """
+
+        cart_id = self.marketplace.new_cart()
+        for i in range(0, 3):  # adauga produse existente intr-un cos existent
+            self.assertFalse(self.marketplace.add_to_cart(cart_id, self.products[i]))
+        # cart_id nu exista, deci nu se poate adauga
+        self.assertFalse(self.marketplace.add_to_cart(cart_id + 10, self.products[10]))
+
+    def test_remove_from_cart(self):
+        """
+        Se creeaza un consumator si se adauga in cos 3 produse din banc. Apoi se incearca stergerea
+        acestor produse iar daca este returnat None, inseamna ca acel produs nu exista in cos.
+        """
+
+        cart_id = self.marketplace.new_cart()
+        for i in range(0, 3):  # adauga produse existente intr-un cos existent
+            self.marketplace.add_to_cart(cart_id, self.products[i])
+
+        for i in range(0, 3):  # sterge produsele adaugate anterior
+            self.assertIsNotNone(self.marketplace.add_to_cart(cart_id, self.products[i]),
+                                 "Can not remove, even though the product is in cart")
+
+    def test_place_order(self):
+        """
+        Se creeaza un consumator si se adauga in cos 3 produse din banc si se salveaza intr-o
+        lista locala produsele adaugate de marketplace. Daca lista returnata nu este aceeasi cu
+        lista locala, atunci se afiseaza mesajul.
+        """
+
+        cart_id = self.marketplace.new_cart()
+        list_order = []
+        for i in range(0, 3):  # adauga produse existente intr-un cos existent
+            self.marketplace.add_to_cart(cart_id, self.products[i])
+            list_order.append(self.products[i])
+
+        self.assertNotEqual(self.marketplace.place_order(cart_id), list_order,
+                            "The lists are not the same!")
